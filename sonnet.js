adf814edc48aea5b1e25a9d60de1cb2afc487f40
Chaz Henricks <chazhenricks@gmail.com>
2017-04-11 13:59:36 -0500

    Completed class exercise

---
+++ sonnet.js
@@ -0,0 +1,37 @@
+/*
+Take the contents of the sonnet div and place it in a variable.
+
+Find and output the starting position of the word "orphans".
+
+Output the number of characters in the sonnet.
+
+Replace the first occurance of the string "winter" with "yuletide".
+
+Replace all occurances of the string "the" with "a large".
+
+Set the content of the sonnet div with the new string.
+*/
+
+
+
+
+var sonnet = document.getElementById("sonnet").innerHTML;
+
+var index = sonnet.indexOf("orphans");
+console.log(index);
+
+var length = sonnet.length;
+
+console.log("length of sonnet", length);
+
+sonnet = sonnet.replace("winter", "yuletide")
+
+console.log("replace winter with yuletide", sonnet);
+
+sonnet = sonnet.replace(/the /gi, 'a large ');
+console.log("replace the with a large", sonnet);
+
+
+
+document.getElementById("sonnet").innerHTML = sonnet;
+
